022f6f29b337a59879aa77c8ae458f0352f1d0f1
trickytommy <trickytommy4@gmail.com>
2020-11-17 09:43:59 +0700

    bottom nav

--- app/src/main/java/com/example/singleactivity/HomeFragment.kt
+++ app/src/main/java/com/example/singleactivity/HomeFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeFragment : Fragment() {
 
+    var navController: NavController? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,7 +36,7 @@ class HomeFragment : Fragment() {
                     true
                 }
                 R.id.navigation_profile -> {
-                    // Respond to navigation item 2 click
+                    navController!!.navigate(R.id.action_homeFragment_to_profileFragment)
                     true
                 }
                 else -> false
